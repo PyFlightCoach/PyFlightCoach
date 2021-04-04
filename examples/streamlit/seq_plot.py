238f2110dff2afa98eab7121fe0fd90b7b9fba70
Thomas David <thomasdavid0@gmail.com>
2021-04-04 11:55:02 +0100

    usb log finder

--- examples/streamlit/seq_plot.py
+++ examples/streamlit/seq_plot.py
@@ -13,6 +13,7 @@ from flightplotting.model import OBJ
 from geometry import Point, Quaternion, Transformation
 import os
 import tkinter as tk
+import easygui
 
 
 st.markdown(
@@ -26,31 +27,37 @@ st.markdown(
         unsafe_allow_html=True,
     )
 
-logfile = 'P21.csv'
+logfile = 'data/logs/144.csv'
 
 if st.sidebar.button("read log csv"):
     try:
         newlog = easygui.fileopenbox(default="/media/tom/storage/shared/flight_logs/")
         if os.path.splitext(newlog)[1] == '.csv':
             logfile = newlog
-    except:
-        pass
+    except Exception as ex:
+        st.write("error: " + str(ex))
 
 bin = Flight.from_csv(logfile)
 
 
 @st.cache  # TODO this may not notice changes to submodules
 def load_data(bin):
-    flight = bin.subset(101, 490)
+    flight = bin.subset(50, 490)
     return flight, Section.from_flight(flight, FlightLine.from_covariance(flight))
 
 
 flight, seq = load_data(bin)
 
+obj = OBJ.from_obj_file('data/models/ColdDraftF3APlane.obj').transform(Transformation(
+    Point(0.75, 0, 0), Quaternion.from_euler(Point(np.pi, 0, -np.pi/2))
+))
 
 
 npoints = st.sidebar.number_input("Number of Models", 0, 50, value=20)
 scale = st.sidebar.number_input("Model Scale Factor", 1.0, 50.0, value=10.0)
+
+scaled_obj = obj.scale(scale)
+
 showmesh = st.sidebar.checkbox("Show Models", False)
 cgtrace = st.sidebar.checkbox("Show CG Trace", True)
 ttrace = st.sidebar.checkbox("Show Tip Trace", False)
@@ -59,15 +66,14 @@ ttrace = st.sidebar.checkbox("Show Tip Trace", False)
 plot_range = st.slider(
     "plot range", 0.0, flight.duration, (0.0, flight.duration))
 
-obj = OBJ.from_file('data/models/model.obj').transform(Transformation(
-    Point(0.75, 0, 0), Quaternion.from_euler(Point(np.pi, 0, -np.pi/2))
-))
+
+
 
 def make_plot_data(seq, plot_range, npoints, showmesh, cgtrace, ttrace):
     sec = seq.subset(*plot_range)
     traces = []
     if showmesh:
-        traces += [mesh for mesh in meshes(obj, npoints, scale, sec)]
+        traces += [mesh for mesh in meshes(scaled_obj, npoints, sec)]
     if cgtrace:
         traces += [trace(sec)]
     if ttrace:
---
+++ scripts/loginput.py
@@ -0,0 +1,81 @@
+'''command line tool to handle flight log input'''
+from os.path import splitext
+import fire
+from flightdata import Flight
+from tkinter import filedialog
+from glob import glob
+import os
+import shutil
+
+def conv(a: str, b: str):
+    """reads an ardupilot bin file and writes it to a csv.
+
+    Args:
+        a (str): the input ardupilot log file
+        b (str): the target csv file
+
+    Returns:
+        [type]: [description]
+    """
+    return Flight.from_log(a).to_csv(b)
+
+
+def brow():
+    """reads an ardupilot bin file and writes it to a csv.
+    opens a file dialog to select the log file and target
+
+    Returns:
+        [type]: [description]
+    """
+
+    a = filedialog.askopenfilename()
+    b = filedialog.asksaveasfile()
+    conv(a, b)
+
+
+def batch():
+    '''asks for a list of bin files, converts them all to csvs in the same folder'''
+    a = filedialog.askopenfilenames()
+    for f in a:
+        try:
+            conv(f, f.replace('.BIN', '.csv'))
+            print(f)
+        except Exception as ex:
+            print("failed to read ", f)
+
+
+def usb():
+    USB_DRIVES = "/media/"
+    LOG_DRIVE = "/home/tom/Desktop/logs/"
+
+    usb_files = glob(USB_DRIVES + "**/*.BIN", recursive = True)
+    disk_files = glob(LOG_DRIVE + "*.BIN")
+
+    disk_filenames = [os.path.basename(file) for file in disk_files]
+    files_to_copy = [file for file in usb_files if not os.path.basename(file) in disk_filenames]
+    print("{0} files to copy".format(len(files_to_copy)))
+    for i, file in enumerate(files_to_copy):
+        print("copying file {0} of {1}".format(i, len(files_to_copy)))
+        shutil.copyfile(file, LOG_DRIVE + os.path.basename(file))
+    
+    disk_csvs = glob(LOG_DRIVE + "*.csv")
+    disk_files = glob(LOG_DRIVE + "*.BIN")
+    csv_names = [os.path.splitext(os.path.basename(file))[0] for file in disk_csvs]
+    files_to_convert = [file for file in disk_files if not os.path.splitext(os.path.basename(file))[0] in csv_names]
+
+    print("{0} files to convert".format(len(files_to_convert)))
+    for i, file in enumerate(files_to_convert):
+        print("converting file {0} to {1}".format(i, len(files_to_convert)))
+        try:
+            conv(file, file.replace(".BIN", ".csv"))
+        except Exception as ex:
+            print(str(ex))
+    
+
+if __name__ == "__main__":
+    fire.Fire({
+        "convert": conv,
+        "browse": brow,
+        "batch": batch,
+        "usb": usb,
+    })
