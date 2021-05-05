e30c55d47c6403431dd8b4784ab126d05fbad363
Thomas David <thomasdavid0@gmail.com>
2021-05-05 13:33:39 +0100

    extend streamlit app functionality

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import tkinter as tk
 from pyflightcoach.log_register.access import new_session
 from pyflightcoach.log_register.tables import Log
 from pathlib import Path
-
+from json import load
 
 register = new_session()
 
@@ -37,31 +37,41 @@ obj = OBJ.from_obj_file('data/models/ColdDraftF3APlane.obj').transform(Transform
     Point(0.75, 0, 0), Quaternion.from_euler(Point(np.pi, 0, -np.pi/2))
 ))
 
-scan_folder = st.sidebar.text_input("folder to scan", "/media/")
-if st.sidebar.button("scan folder"):
-    register.register_folder(Path(scan_folder))
+with st.sidebar.beta_expander("log selection"):
+    scan_folder = st.text_input("folder to scan", "/media/")
+    if st.button("scan folder"):
+        register.register_folder(Path(scan_folder))
 
-fp = st.sidebar.file_uploader("select bin file", "BIN")
-if fp:
-    log = register.register_log(fp)
-else:
-    log = register.latest_log()
-
-
-
-
-flightline = FlightLine.from_box(Box.from_json(
-    'examples/notebooks/flightlines/gordano_box.json'))
-
-def load_data(log):
-    bin = log.flight()  
-    return bin, Section.from_flight(bin, flightline)
-
+    fp = st.file_uploader("select bin file", "BIN")
+    if fp:
+        log = register.register_log(fp)
+    else:
+        log = register.latest_log()
 
 loading = st.empty()
 loading.text("reading log .....")
 
-flight, seq = load_data(log)
+flight = log.flight()  
+
+with st.sidebar.beta_expander("flightline setup"):
+    fltype = st.radio("method", ["json", "covariance", "initial position"], 0)
+    if fltype == "json":
+        fp2 = st.file_uploader("select flightline json", "json")
+        if not fp2:
+            box = Box.from_json('examples/notebooks/flightlines/gordano_box.json')
+        else:
+            box = Box(**load(fp2))
+        flightline = FlightLine.from_box(box)
+    elif fltype=="covariance":
+        flightline = FlightLine.from_covariance(flight)
+    elif fltype=="initial position":
+        flightline = FlightLine.from_initial_position(flight)
+
+loading.text("moving to flightline .....")
+
+
+seq = Section.from_flight(flight, flightline)
+
 loading.empty()
 
 npoints = st.sidebar.number_input("Number of Models", 0, 100, value=40)
@@ -74,6 +84,9 @@ ttrace = st.sidebar.checkbox("Show Tip Trace", True)
 btrace = st.sidebar.checkbox("Show Box Trace", True)
 
 perspective = st.sidebar.checkbox("perspective", True)
+
+
+
 plot_range = st.slider(
     "plot range", 0.0, flight.duration, (0.0, flight.duration))
 
@@ -110,4 +123,5 @@ st.plotly_chart(
 sequence = st.sidebar.text_input("Enter Sequence Name", log.sequence.name if log.sequence else "Unknown")
 if st.sidebar.button("save sequence selection"):
     register.set_sequence(log, sequence)
+
 
