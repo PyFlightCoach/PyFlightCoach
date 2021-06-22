56e5a4a6cd39568d214204962e0be6875ee820b8
Thomas David <thomasdavid0@gmail.com>
2021-06-22 12:16:50 +0100

    flight coach templates

--- pyflightcoach/create_FC_json.py
+++ pyflightcoach/create_FC_json.py
@@ -5,13 +5,15 @@ from geometry import Points, Quaternions, Transformation, Coord, Point
 from json import dump, load
 from io import open
 
-def create_json(sec: Section, file: str):
+def create_json(sec: Section, file: str, header: str):
     """Create a json file to input into the flight coach plotter (https://www.flightcoach.org/ribbon/plotter.html)
     
 
     Args:
         sec (Section): [description]
     """
+    print("creating {}".format(file))
+    print("creating 6d data")
     fcdata = pd.DataFrame(columns=["N","E","D","VN","VE","VD","r","p","yw","wN","wE","roll","pitch","yaw"])
 
     fcdata["N"] = sec.x
@@ -48,17 +50,25 @@ def create_json(sec: Section, file: str):
     fcdata.columns = ["time", "N","E","D","VN","VE","VD","r","p","yw","wN","wE","roll","pitch","yaw"]
     dout = fcdata.to_dict("records")
 
-    with open("data/FCJsonHeader.json", "r") as f:
+    with open(header, "r") as f:
         data = load(f)
 
     data["data"] = dout
 
-    temp = sec.data.reset_index()
-    data["mans"][0]["stop"]=1
-    for tman, fcman in zip(sec.manoeuvre.unique(), data["mans"][1:]):
-        fcman["stop"] = int(temp.loc[temp.manoeuvre == tman].index[-1] + 1)
-        fcman["start"] = int(temp.loc[temp.manoeuvre == tman].index[0])
+    print("populating splitter")
 
+    sec.data.loc[:3.5, ["manoeuvre"]] = "tkoff"
+
+    temp = sec.data.reset_index()
+    
+    for tman, fcman in zip(sec.manoeuvre.unique(), data["mans"]):
+        fcman["stop"] = int(temp.loc[temp.manoeuvre == tman].index[-1])+1
+        fcman["start"] = int(temp.loc[temp.manoeuvre == tman].index[0])
+        fcman["wd"]=100*sec.get_manoeuvre(tman).duration / sec.duration
+
+    data["mans"][0]["wd"] = data["mans"][0]["wd"] + 0.2
+
+    print("saving file")
     with open(file, "w") as f:
         dump(data, f)
 
@@ -66,5 +76,13 @@ def create_json(sec: Section, file: str):
 if __name__ == "__main__":
     from flightanalysis.schedule.p21 import *
     from flightanalysis.schedule.p23 import *
-    create_json(Section.from_schedule(p23, 170.0, "left"), "P23_template.json")
-    create_json(Section.from_schedule(p21, 170.0, "left"), "P21_template.json")
+    from flightanalysis.schedule.f21 import *
+    from flightanalysis.schedule.f23 import *
+    create_json(Section.from_schedule(p23, 150.0, "left"), "P23_template_150.json", "data/FCJsonHeader_p23.json")
+    create_json(Section.from_schedule(p21, 150.0, "left"), "P21_template_150.json", "data/FCJsonHeader_p21.json")
+    create_json(Section.from_schedule(f21, 150.0, "right"), "F21_template_150.json", "data/FCJsonHeader_f21.json")
+    create_json(Section.from_schedule(f23, 150.0, "left"), "F23_template_150.json", "data/FCJsonHeader_f23.json")
+    create_json(Section.from_schedule(p23, 170.0, "left"), "P23_template_170.json", "data/FCJsonHeader_p23.json")
+    create_json(Section.from_schedule(p21, 170.0, "left"), "P21_template_170.json", "data/FCJsonHeader_p21.json")
+    create_json(Section.from_schedule(f21, 170.0, "right"), "F21_template_170.json", "data/FCJsonHeader_f21.json")
+    create_json(Section.from_schedule(f23, 170.0, "left"), "F23_template_170.json", "data/FCJsonHeader_f23.json")
