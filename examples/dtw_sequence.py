c812219f54a37e0b84a85b39d512852d633499a7
Thomas David <thomasdavid0@gmail.com>
2021-07-01 22:48:21 +0100

    labelling changes

--- examples/dtw_sequence.py
+++ examples/dtw_sequence.py
@@ -9,20 +9,21 @@ from flightplotting.model import OBJ
 from geometry import Transformation, Quaternion, Point
 from io import open
 from json import load
-import flightanalysis.schedule.p21 as sched
+from flightanalysis.schedule import p21
 import numpy as np
 import pandas as pd
 
 obj = OBJ.from_obj_file('data/models/ColdDraftF3APlane.obj').transform(Transformation(
-    Point(0.75, 0, 0), Quaternion.from_euler(Point(np.pi, 0, -np.pi/2))
+    Point(0.75, 0, 0), Quaternion.from_euler(Point(np.pi, 0, -np.pi / 2))
 ))
 
 
-template = Section.from_schedule(sched.p21, 170.0, "left")
+template = p21.create_template("left", 170.0)
 
 
 flight = Flight.from_csv("data/logs/flight_csvs/00000136.csv")
-flown = Section.from_flight(flight, FlightLine.from_covariance(flight)).subset(102, 490)  # 490
+flown = Section.from_flight(
+    flight, FlightLine.from_covariance(flight)).subset(102, 490)  # 490
 
 
 plotsec(template, obj, 7, 100, color='orange').show()
@@ -32,9 +33,9 @@ plotsec(flown, obj, 6.0).show()
 
 fig = make_subplots(rows=2, cols=1)
 for tr in axis_rate_trace(flown, True):
-   fig.add_trace(tr,row=1, col=1)
+    fig.add_trace(tr, row=1, col=1)
 for tr in axis_rate_trace(template, True):
-   fig.add_trace(tr,row=2, col=1)
+    fig.add_trace(tr, row=2, col=1)
 fig.show()
 
 
@@ -42,11 +43,11 @@ distance, aligned = Section.align(flown, template)
 print(distance)
 
 go.Figure(
-   data=manoeuvretraces(aligned),
-   layout=go.Layout(template="flight3d+judge_view")
-   ).show()
+    data=manoeuvretraces(p21, aligned),
+    layout=go.Layout(template="flight3d+judge_view")
+).show()
 
 fig = go.Figure(layout=go.Layout(template="flight3d"))
-for manoeuvre in aligned.manoeuvre.unique():
-   fig.add_traces(elementtraces(aligned.get_manoeuvre(manoeuvre)))
+for manoeuvre in p21.manoeuvres:
+    fig.add_traces(elementtraces(manoeuvre, aligned))
 fig.show()
