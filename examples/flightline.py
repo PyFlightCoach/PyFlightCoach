e85c41879818690c5b3e6bda1b3422878b20bc27
Thomas David <thomasdavid0@gmail.com>
2021-07-20 10:05:43 +0100

    axis rate matching

--- examples/flightline.py
+++ examples/flightline.py
@@ -6,10 +6,10 @@ pilot = Flight.from_log("data/logs/flightlines/pilot_pos.BIN")
 centre = Flight.from_log("data/logs/flightlines/centre_point.BIN")
 
 
-_p = pilot.read_fields(Fields.GLOBALPOSITION).mean()
+_p = pilot.read_fields(Fields.GLOBALPOSITION).iloc[-1]
 p = GPSPosition(_p.global_position_latitude, _p.global_position_longitude)
 
-_c = centre.read_fields(Fields.GLOBALPOSITION).mean()
+_c = centre.read_fields(Fields.GLOBALPOSITION).iloc[-1]
 c = GPSPosition(_c.global_position_latitude, _c.global_position_longitude)
 
 
@@ -18,9 +18,11 @@ box = Box.from_points("evening_normal", p,c)
 box.club = "BRCMAC"
 box.country = "UK"
 
+
+
 print(p)
 print(c)
 
 
 print(box)
-box.to_json("data/flightlines/duckhole.json")
+box.to_json("data/flightlines/duckhole_evening_normal.json")
