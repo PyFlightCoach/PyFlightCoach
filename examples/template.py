5e6643c7e9d1625dbe488e2a116154efe3ac4d83
Thomas David <thomasdavid0@gmail.com>
2021-10-01 14:11:19 +0100

    tidying

--- examples/template.py
+++ examples/template.py
@@ -1,9 +1,8 @@
 from flightanalysis import get_schedule, Categories
 from flightplotting.plots import plotsec
-from examples.model import obj
 
 schedule = get_schedule(Categories.F3A, "P21").scale_distance(170)
 template = schedule.create_raw_template("left", 30.0, 170)
 
 
-plotsec(template, obj).show()
+plotsec(template).show()
