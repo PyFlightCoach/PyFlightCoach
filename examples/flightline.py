453e439d24d664bde013212cf572ddaa89fefd5b
Thomas David <thomasdavid0@gmail.com>
2021-11-03 10:49:52 +0000

    flightline tool and template updates

--- examples/flightline.py
+++ examples/flightline.py
@@ -1,29 +1,56 @@
 from flightdata import Flight, Fields
 from geometry import GPSPosition
-from flightanalysis.flightline import Box
+from flightanalysis import Box, Section
+import easygui
+import sys
 
-#pilot = Flight.from_log("data/logs/flightlines/pilot_pos.BIN")
-#"/mnt/D/APM/LOGS/00000100.BIN"
-#"/mnt/c/projects/flight_analysis/logs/"
-pilot = Flight.from_log("/mnt/c/projects/flight_analysis/logs/00000111.BIN")
-
-#centre = Flight.from_log("data/logs/flightlines/centre_point.BIN")
-centre = Flight.from_log("/mnt/c/projects/flight_analysis/logs/00000112.BIN")
+from scipy.cluster.vq import kmeans
 
 
-p = GPSPosition(*pilot.read_fields(Fields.GLOBALPOSITION).iloc[-1])
+def path_or_browse(instruction, meth = easygui.fileopenbox):
+    pilot_pos = input(instruction)
+    if not pilot_pos:
+        pilot_pos = meth()
+    return pilot_pos
 
-c = GPSPosition(*centre.read_fields(Fields.GLOBALPOSITION).iloc[-1])
+flight = None
+if input("use channel 5 switch positions?") in ["y", "Y", "yes"]:
+    flight = Flight.from_log(path_or_browse("Pilot Position Bin path or empty for browse\n"))#path_or_browse("Flight Log Bin path or empty for browse\n"))
+    c6on = Flight(flight.data.loc[flight.data.tx_controls_5>=1500])
+    res = kmeans(c6on.read_fields(Fields.GLOBALPOSITION), 2)[0]
 
 
-box = Box.from_points("example_box", p,c)
+    p = GPSPosition(*res[0])
+    c = GPSPosition(*res[1])
 
+else:
+    pilot = Flight.from_log(path_or_browse("Pilot Position Bin path or empty for browse\n"))
+    centre = Flight.from_log(path_or_browse("Centre Position Bin path or empty for browse\n"))
+    p = GPSPosition(*pilot.read_fields(Fields.GLOBALPOSITION).iloc[-1])
+    c = GPSPosition(*centre.read_fields(Fields.GLOBALPOSITION).iloc[-1])
+
+
+box = Box.from_points("new", p, c)
 
 print(p)
 print(c)
+print(box.to_dict())
 
-from json import dump
-from io import open
-with open("temp.json", "w") as f:
-    dump(box.to_dict(), f)
+if input("create section csv?\n") in ["y", "Y", "yes"]:
+    if not flight is None:
+        use_current = input("use current log?\n")
+    if not use_current in ["y", "Y", "yes"]:
+        flight = Flight.from_log(path_or_browse("Flight Log BIN File\n"))
 
+    tx = flight.read_fields(Fields.TXCONTROLS)
+    tx = tx - tx.iloc[0]
+    tx = tx.iloc[:,:5]
+    tx.columns = ["throttle", "aileron_1", "aileron_2", "elevator", "rudder"]
+
+    sec = Section.from_flight(flight, box).append_columns(tx)
+
+    sec.to_csv(path_or_browse("Save section csv location\n", easygui.filesavebox))
+#
+#
+#
+#
