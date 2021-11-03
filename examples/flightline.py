92a1f5b14a4131db55eaa4b979e9e06da88ded53
Thomas David <thomasdavid0@gmail.com>
2021-11-03 06:57:46 +0000

    flightline ch6

--- examples/flightline.py
+++ examples/flightline.py
@@ -3,10 +3,10 @@ from geometry import GPSPosition
 from flightanalysis.flightline import Box
 
 #pilot = Flight.from_log("data/logs/flightlines/pilot_pos.BIN")
-pilot = Flight.from_log("/media/tom/LOGS/APM/LOGS/00000078.BIN")
+pilot = Flight.from_log("/media/tom/LOGS/APM/LOGS/00000118.BIN")
 
 #centre = Flight.from_log("data/logs/flightlines/centre_point.BIN")
-centre = Flight.from_log("/media/tom/LOGS/APM/LOGS/00000079.BIN")
+centre = Flight.from_log("/media/tom/LOGS/APM/LOGS/00000119.BIN")
 
 
 p = GPSPosition(*pilot.read_fields(Fields.GLOBALPOSITION).iloc[-1])
@@ -20,8 +20,8 @@ box = Box.from_points("example_box", p,c)
 print(p)
 print(c)
 
-from json import dump
-from io import open
-with open("temp.json", "w") as f:
-    dump({"pilot": p.to_dict(), "center": c.to_dict()}, f)
+#from json import dump
+#from io import open
+#with open("temp.json", "w") as f:
+#    dump({"pilot": p.to_dict(), "center": c.to_dict()}, f)
 
