530b9246d07f8fd9e053373d115d06d2332d6b5f
Thomas David <thomasdavid0@gmail.com>
2022-03-08 14:52:35 +0000

    tidying

--- examples/template.py
+++
@@ -1,8 +0,0 @@
-from flightanalysis import get_schedule, Categories
-from flightplotting.plots import plotsec
-
-schedule = get_schedule(Categories.F3A, "P21").scale_distance(170)
-template = schedule.create_raw_template("left", 30.0, 170)
-
-
-plotsec(template).show()
